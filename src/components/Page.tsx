8b4c6bd39977360d73844728e87c43c5f3cb1b70
Ubuntu <ubuntu@ip-172-31-21-38.ec2.internal>
2022-11-26 10:00:19 +0000

    first commit

---
+++ src/components/Page.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import {
+    Button,
+    H6, H5,
+    Colors,
+    Classes,
+    Checkbox,
+    Navbar,
+    NavbarHeading,
+    NavbarGroup,
+    NavbarDivider,
+    Icon,
+    Intent,
+    IconSize,
+    ControlGroup,
+    InputGroup} from "@blueprintjs/core";
+
+export class Page extends React.Component {
+
+    render() {
+        return (
+            <div className={"bp4-dark"}>
+                TEST
+            </div>
+        )
+    }
+}
